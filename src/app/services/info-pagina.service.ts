9b63a395e237014468b6e6470d5e74e02699aef7
gnomofeo <victortadeo2011xd@hotmail.com>
2021-06-18 02:29:14 -0700

    seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,14 +10,31 @@ export class InfoPaginaService {
 
   info:InfoPagina ={};
   cargada =  false;
+  equipo: any[] = [];
   constructor(private http:HttpClient) {
-    console.log('servicio de infoPagina listo');
-    //leer el archivo json
+  
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
+
+   private cargarInfo(){
+ 
     this.http.get('assets/Data/Data-pagina.json')
     .subscribe((resp:  InfoPagina) => {
       this.cargada=true;
       this.info  = resp;
   
-    })
-   }
+    });
+
+  }
+  private cargarEquipo(){
+    this.http.get('https://angular-html-d8ed4-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp:any[])  => {
+
+      this.equipo  = resp;
+      console.log(resp);
+    });
+  }
+
 }
+
